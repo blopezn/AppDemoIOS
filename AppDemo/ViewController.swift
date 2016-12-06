c0e760988a89a1a1584abaa51e9b8ef025246e55
adminisitradorUTM <adminisitradorutm@imac-de-adminisitradorutm.local>
2016-12-06 09:49:03 -0600

    Primera versión de la App del curso iOS

--- AppDemo/ViewController.swift
+++ AppDemo/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let datos = [("Luis", 30), ("Kike", 28)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Vista cargada")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datos.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let proto = (indexPath.row % 2 == 0) ? "proto1" : "proto2"
+        
+        
+        let vista = tableView.dequeueReusableCell(withIdentifier: proto, for: indexPath) as! FilaTableViewCell
+        
+        if(indexPath.row % 2 == 0){
+            vista.lblIzquierda.text = datos[indexPath.row].0
+        } else {
+            vista.lblIzquierda.text = datos[indexPath.row].0
+            vista.lblDerecha.text = "\(datos[indexPath.row].1)"
+        }
+        
+        return vista
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("File \(indexPath.row)")
+    }
 }
 
